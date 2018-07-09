5dfc28b8b980ab6c2c44e21835d48cc9e59de90e
Jeremy Parker <jrobinparker@gmail.com>
2018-07-09 17:30:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeli = []
 
-function takeANumber(person) {
+function takeANumber(katzDeli, person) {
   if (katzDeli.length === 0) {
     katzDeli.unshift(person)
   } else {
@@ -8,4 +8,19 @@ function takeANumber(person) {
   }
 }
 
-functin
+function nowServing(katzDeli, person) {
+  if (katzDeli.length === 0) {
+    return "The line is currently empty."
+  } else {
+    return "Now serving ${katzDeli[0]}";
+    katzDeli.shift(person)
+  }
+}
+
+function currentLine(katzDeli) {
+  if (katzDeli.length === 0) {
+    return "The line is currently empty."
+  } else {
+    
+  }
+}
