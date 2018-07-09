5180a3cd1e435fef48a2141d538ffc72b9c047f7
Jeremy Parker <jrobinparker@gmail.com>
2018-07-09 17:35:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function nowServing(katzDeli) {
     return "There is nobody waiting to be served!"
   } else {
     var beingServed = katzDeli.shift();
-    return "Currently serving ${beingServed}"
+    return "Currently serving ${beingServed}."
   }
 }
 
