0937909dff9e0421752d2b6b6a3e4d6c1fa28354
Jeremy Parker <jrobinparker@gmail.com>
2018-07-09 19:07:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function nowServing(katzDeli) {
   }
 }
 
-function currentLine(katzDeli, person) {
+function currentLine(katzDeli) {
   var line = []
   if (katzDeli.length === 0) {
     return "The line is currently empty."
